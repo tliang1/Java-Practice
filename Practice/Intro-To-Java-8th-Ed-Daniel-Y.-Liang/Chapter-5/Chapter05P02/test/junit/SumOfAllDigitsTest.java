5667e15733807df9fb837bba8c54e464146fd5d9
tliang <masterrs5000@yahoo.com>
2016-10-20 18:23:26 -0700

    Add Chapter 5 Problem 2 project

---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-5/Chapter05P02/test/junit/SumOfAllDigitsTest.java
@@ -0,0 +1,44 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import main.SumOfAllDigits;
+
+/**
+ * @author Tony Liang
+ *
+ */
+@RunWith(Parameterized.class)
+public class SumOfAllDigitsTest
+{
+	private long n;
+	private long expected;
+	
+	public SumOfAllDigitsTest(long number, long expectedResult)
+	{
+		this.n = number;
+		this.expected = expectedResult;
+	}
+	
+	@Parameters
+	public static Collection<Integer[]> sumDigitsList() 
+	{
+		return Arrays.asList(new Integer[][] { { -7892, -26 }, { -756, -18 }, { -48, -12 }, { 1, 1 }, { 25, 7 }, 
+			{ 234, 9 }, { 999, 27 }, { 2569, 22 }, { 8036, 17 } });
+	}
+	
+	@Test
+	public void testSumDigits()
+	{
+		assertEquals(expected, SumOfAllDigits.sumDigits(n));
+	}
+
+}
