38091d0afa876909480c6caa15954dcf829bf521
tliang <masterrs5000@yahoo.com>
2016-10-22 22:41:54 -0700

    Add Chapter 5 Problem 27 project

---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-5/Chapter05P27/src/main/Emirps.java
@@ -0,0 +1,125 @@
+package main;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class Emirps
+{
+	
+	public static void main(String[] args)
+	{
+		printEmirps(100, 10);
+	}
+
+	/**
+	 * Returns if the number is prime.
+	 * <p>
+	 * If the integer argument is less than 2, the number will be considered not prime.
+	 * 
+	 * @param number	a number
+	 * @return			true if number is prime. Otherwise, false.
+	 */
+	public static boolean isPrime(int number)
+	{
+		if (number < 2)
+		{
+			return false;
+		}
+		
+		for (int divisor = 2; divisor <= number / 2; divisor++) 
+		{
+			if (number % divisor == 0) 
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Returns the reverse of an integer.
+	 * 
+	 * @param number	an integer
+	 * @return			reverse of the integer
+	 */
+	public static int reverse(int number)
+	{
+		if (number == 0)
+		{
+			return 0;
+		}
+		
+		String reverseNumberString = "";
+		
+		if (number < 0)
+		{
+			reverseNumberString = "-";
+		}
+		
+		while (number != 0)
+		{
+			reverseNumberString += (int)(Math.abs((number % 10)));
+			number /= 10;
+		}
+		
+		return Integer.parseInt(reverseNumberString);
+	}
+	
+	/**
+	 * Returns if an integer is a palindrome integer.
+	 * 
+	 * @param number	an integer
+	 * @return			true if integer is palindrome. Otherwise, false.
+	 */
+	public static boolean isPalindrome(int number)
+	{
+		return number == reverse(number);
+	}
+	
+	/**
+	 * Prints up to the number of emirp numbers specified by the first argument while limiting the numbers per 
+	 * line by the second argument.
+	 * <p>
+	 * If the first argument is less than 1, no emirp numbers are displayed.
+	 * <p>
+	 * If the second argument is less than 1, it will default to 1.
+	 * 
+	 * @param numberOfEmirps	total number of emirp numbers to display
+	 * @param maxEmirpsPerLine	max emirp numbers to display per line
+	 */
+	public static void printEmirps(int numberOfEmirps, int maxEmirpsPerLine)
+	{
+		if (maxEmirpsPerLine < 1)
+		{
+			maxEmirpsPerLine = 1;
+		}
+		
+		int count = 1;
+		int number = 2;
+		
+		while (count <= numberOfEmirps)
+		{
+			if (isPrime(number))
+			{
+				if (!isPalindrome(number))
+				{
+					if (isPrime(reverse(number)))
+					{
+						System.out.printf("%4d ", number);
+						
+						if ((count % maxEmirpsPerLine == 0) || (count == numberOfEmirps))
+						{
+							System.out.println();
+						}
+						
+						count++;
+					}
+				}
+			}
+			
+			number++;
+		}
+	}
+}
