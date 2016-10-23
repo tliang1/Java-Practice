c7ff83d09f5b30cbfd31fa63819089911b4b1632
tliang <masterrs5000@yahoo.com>
2016-10-22 21:50:18 -0700

    Add Chapter 5 Problem 22 project

---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-5/Chapter05P22/src/main/ApproximateSqrt.java
@@ -0,0 +1,54 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class ApproximateSqrt
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		double num = -1.0;
+		
+		while (num < 0.0)
+		{
+			System.out.print("Enter a positive number: ");
+			num = input.nextDouble();
+		}
+		
+		System.out.println("The square root of " + num + " is approximately " + sqrt(num));
+		
+		input.close();
+	}
+
+	/**
+	 * Returns the approximated square root of the number.
+	 * <p>
+	 * If the number is negative, the default value 0.0 will be returned.
+	 * 
+	 * @param num	a positive number
+	 * @return		the approximated square root of the number
+	 */
+	public static double sqrt(double num)
+	{
+		if (num < 0.0)
+		{
+			return 0.0;
+		}
+		
+		double lastGuess = 1.0;
+		double nextGuess = (lastGuess + (num / lastGuess)) / 2.0;
+		
+		while (Math.abs(nextGuess - lastGuess) >= 0.0001)
+		{
+			lastGuess = nextGuess;
+			nextGuess = (lastGuess + (num / lastGuess)) / 2.0;
+		}
+		
+		return nextGuess;
+	}
+}
