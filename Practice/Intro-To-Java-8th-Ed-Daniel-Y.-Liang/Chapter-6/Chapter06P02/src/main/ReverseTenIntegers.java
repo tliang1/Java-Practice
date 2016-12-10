35c54e92b714c8abd781423ea3d53dc5cd8a0592
tliang <masterrs5000@yahoo.com>
2016-12-09 21:27:22 -0800

    Add Chapter 6 Problem 2 project

---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P02/src/main/ReverseTenIntegers.java
@@ -0,0 +1,61 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class ReverseTenIntegers
+{
+	public static void main(String[] args)
+	{	
+		Scanner input = new Scanner(System.in);
+		int[] integers = new int[10];
+		
+		System.out.print("Enter ten integers: ");
+		
+		for(int count = 0; count < integers.length; count++)
+		{
+			integers[count] = input.nextInt();
+		}
+		
+		int[] reverseIntegers = reverse(integers);
+		
+		System.out.print("The ten integers in reverse are ");
+		
+		for(int count = 0; count < reverseIntegers.length; count++)
+		{
+			System.out.print(reverseIntegers[count] + " ");
+		}
+		
+		System.out.println();
+		
+		input.close();
+	}
+	
+	/**
+	 * Returns the integers in reverse order given the integer array argument.
+	 * <p>
+	 * If the integer array's size is less than 2, the integer array is returned as is.
+	 * 
+	 * @param list	array of integers
+	 * @return		the array of integers in reverse order
+	 */
+	public static int[] reverse(int[] list)
+	{
+		if (list.length < 2)
+		{
+			return list;
+		}
+		
+		int[] result = new int[list.length];
+		
+		for (int i = 0, j = result.length - 1; i < list.length; i++, j--)
+		{
+			result[j] = list[i];
+		}
+		
+		return result;
+	}
+}
