4197fe589643078eb04e88a0c86f0589ad6cfd26
tliang <masterrs5000@yahoo.com>
2016-12-11 17:36:52 -0800

    Add Chapter 6 Problem 5 project

---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P05/src/main/DistinctNumbers.java
@@ -0,0 +1,62 @@
+package main;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class DistinctNumbers
+{
+	
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int[] integers = new int[10];
+		Arrays.fill(integers, -1);
+		
+		System.out.print("Enter ten numbers: ");
+		
+		int distinctIndex = 0;
+		
+		for(int count = 0; count < integers.length; count++)
+		{
+			int number = input.nextInt();
+			
+			boolean distinct = true;
+			
+			for (int index = 0; index < (count + 1); index++)
+			{
+				if (integers[index] == number)
+				{
+					distinct = false;
+					break;
+				}
+			}
+			
+			if (distinct)
+			{
+				integers[distinctIndex] = number;
+				distinctIndex++;
+			}
+		}
+		
+		System.out.print("The distinct numbers are: ");
+		
+		for (int index = 0; index < integers.length; index++)
+		{
+			if (integers[index] < 0)
+			{
+				break;
+			}
+			
+			System.out.print(integers[index] + " ");
+		}
+		
+		System.out.println();
+		
+		input.close();
+	}
+}
