9c66d80b1bc6614c0b8df1c81cc96e565b03ec1d
tliang <masterrs5000@yahoo.com>
2016-12-12 21:00:20 -0800

    Add Chapter 6 Problem 12 project

---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P12/src/main/ReversingAnArray.java
@@ -0,0 +1,61 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class ReversingAnArray
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		final int NUMBER_OF_INTEGERS = 10;
+		int[] integers = new int[NUMBER_OF_INTEGERS];
+		
+		System.out.print("Enter ten integers: ");
+		
+		for(int count = 0; count < integers.length; count++)
+		{
+			integers[count] = input.nextInt();
+		}
+		
+		integers = reverse(integers);
+		
+		for(int count = 0; count < integers.length; count++)
+		{
+			System.out.print(integers[count] + " ");
+		}
+		
+		System.out.println();
+		
+		input.close();
+	}
+	
+	/**
+	 * Returns the integers in reverse order given the integer array argument.
+	 * <p>
+	 * If the integer array's size is less than 2, the integer array is returned as is.
+	 * 
+	 * @param list	array of integers
+	 * @return		the array of integers in reverse order
+	 */
+	public static int[] reverse(int[] list)
+	{
+		if (list.length < 2)
+		{
+			return list;
+		}
+		
+		for (int i = 0, j = list.length - 1; i < (list.length / 2); i++, j--)
+		{
+			int temp = list[i];
+			list[i] = list[j];
+			list[j] = temp;
+		}
+		
+		return list;
+	}
+}
---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P12/test/junit/ReversingAnArrayTest.java
@@ -0,0 +1,28 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.ReversingAnArray;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class ReversingAnArrayTest
+{
+	int[][] integers = new int[][] { { }, { 4 }, { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 }, 
+		{ 3, 0, 34, 7, -8, 2, -2, 5, 25 } };
+	int[][] reverseIntegers = new int[][] { { }, { 4 }, { 10, 8, 6, 4, 2, 9, 7, 5, 3, 1 }, 
+		{ 25, 5, -2, 2, -8, 7, 34, 0, 3 } };
+		
+	@Test
+	public void test()
+	{
+		for (int index = 0; index < integers.length; index++)
+		{
+			assertArrayEquals(reverseIntegers[index], ReversingAnArray.reverse(integers[index]));
+		}
+	}
+}
