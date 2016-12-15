a2b77f6b954f7af98ee60b9225acc3d924d1a377
tliang <masterrs5000@yahoo.com>
2016-12-14 19:37:58 -0800

    Add more test cases

--- Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P14/test/junit/ComputeGCDTest.java
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P14/test/junit/ComputeGCDTest.java
@@ -12,9 +12,9 @@ import main.ComputeGCD;
  */
 public class ComputeGCDTest
 {
-	int[][] numbers = { { }, { 7 }, { -64, 46, 92, -12, -2 }, 
+	int[][] numbers = { { }, { 0, 0, 0, 0 }, { 7 }, { -64, 46, 92, -12, -2 }, { 20, 10, 0, 5, 0, 15, 30 },
 			{ 208, 156, 52, 104, 130, 312, 117, 234, 182, 351, 338, 26, 286 } };
-	int[] gcds = { 0, 7, 2, 13 };
+	int[] gcds = { 0, 0, 7, 2, 5, 13 };
 	
 	@Test
 	public void testGCD()
