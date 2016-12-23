40f6d922247f239d470ba06c74111671b792b61a
tliang <masterrs5000@yahoo.com>
2016-12-22 22:18:10 -0800

    Add Chapter 6 Problem 30 project

---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P30/src/main/FourSameConsecutiveNumbers.java
@@ -0,0 +1,84 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class FourSameConsecutiveNumbers
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the input size: ");
+		int inputSize = input.nextInt();
+		
+		while (inputSize < 1)
+		{
+			System.out.print("Enter the input size (Must be nonzero positive): ");
+			inputSize = input.nextInt();
+		}
+		
+		int[] values = new int[inputSize];
+		
+		System.out.print("Enter " + inputSize + " integers: ");
+		
+		for(int count = 0; count < inputSize; count++)
+		{
+			values[count] = input.nextInt();
+		}
+		
+		System.out.print("The series of integers ");
+		
+		if (!isConsecutiveFour(values))
+		{
+			System.out.print("does not ");
+		}
+		
+		System.out.println("contains four consecutive numbers with the same value.");
+		
+		input.close();
+	}
+	
+	/**
+	 * Returns if four consecutive integers with the same value is in the integer array argument.
+	 * <p>
+	 * If the integer array's size is less than 4, it will default to false.
+	 * 
+	 * @param values	array of integers
+	 * @return			true if four consecutive integers with the same value is in the integer array argument.
+	 * 					Otherwise, false.
+	 */
+	public static boolean isConsecutiveFour(int[] values)
+	{
+		if (values.length < 4)
+		{
+			return false;
+		}
+		
+		int currentConsecutiveNumber = values[0];
+		int countConsecutiveNumber = 1;
+		
+		for (int index = 1; index < values.length; index++)
+		{
+			if (values[index] == currentConsecutiveNumber)
+			{
+				countConsecutiveNumber++;
+				
+				if (countConsecutiveNumber == 4)
+				{
+					return true;
+				}
+			}
+			else
+			{
+				currentConsecutiveNumber = values[index];
+				countConsecutiveNumber = 1;
+			}
+		}
+		
+		return false;
+	}
+}
---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P30/test/junit/FourSameConsecutiveNumbersTest.java
@@ -0,0 +1,28 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.FourSameConsecutiveNumbers;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class FourSameConsecutiveNumbersTest
+{
+	int[][] listOfValues = { { }, { 1 }, { 2, 3 }, { 4, 5, 6 }, { 7, 8, 9, 10 }, { 4, 4, 4, 4 },
+			{ 3, 2, 2, 2, 5, 4, 1, 6 }, { 7, 7, 7, 7, 9, 8, 1, 6 }, { 5, 1, 9, 9, 9, 9, 7, 1 }, 
+			{ 8, 2, 1, 3, 3, 3, 6, 3, 3, 3, 3, 3, 9 } };
+	boolean[] consecutiveOrNot = { false, false, false, false, false, true, false, true, true, true };
+	
+	@Test
+	public void testIsConsecutiveFour()
+	{
+		for (int list = 0; list < listOfValues.length; list++)
+		{
+			assertEquals(consecutiveOrNot[list], FourSameConsecutiveNumbers.isConsecutiveFour(listOfValues[list]));
+		}
+	}
+}
