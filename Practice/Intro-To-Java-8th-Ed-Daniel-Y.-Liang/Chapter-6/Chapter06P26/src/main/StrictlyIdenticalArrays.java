6452f8fc1f25bdcbee46f5eb00067a410a405742
tliang <masterrs5000@yahoo.com>
2016-12-22 16:41:08 -0800

    Add Chapter 6 Problem 26 project

---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P26/src/main/StrictlyIdenticalArrays.java
@@ -0,0 +1,84 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class StrictlyIdenticalArrays
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter list1: ");
+		int list1Size = input.nextInt();
+		
+		if (list1Size < 0)
+		{
+			list1Size = 0;
+		}
+		
+		int[] list1 = new int[list1Size];
+		
+		for (int index = 0; index < list1Size; index++)
+		{
+			list1[index] = input.nextInt();
+		}
+		
+		System.out.print("Enter list2: ");
+		int list2Size = input.nextInt();
+		
+		if (list2Size < 0)
+		{
+			list2Size = 0;
+		}
+		
+		int[] list2 = new int[list2Size];
+		
+		for (int index = 0; index < list2Size; index++)
+		{
+			list2[index] = input.nextInt();
+		}
+		
+		System.out.print("Two lists are ");
+		
+		if (!equal(list1, list2))
+		{
+			System.out.print("not ");
+		}
+		
+		System.out.println("strictly identical");
+		
+		input.close();
+	}
+	
+	/**
+	 * Returns if the two integer array arguments are strictly identical.
+	 * <p>
+	 * If the two integer arrays' sizes are not equal, the integer arrays will default to being not strictly
+	 * identical.
+	 * 
+	 * @param list1		array of integers
+	 * @param list2		array of integers
+	 * @return			true if the two lists are strictly identical. Otherwise, false.
+	 */
+	public static boolean equal(int[] list1, int[] list2)
+	{
+		if (list1.length != list2.length)
+		{
+			return false;
+		}
+		
+		for (int index = 0; index < list1.length; index++)
+		{
+			if (list1[index] != list2[index])
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
---
+++ Practice/Intro-To-Java-8th-Ed-Daniel-Y.-Liang/Chapter-6/Chapter06P26/test/junit/StrictlyIdenticalArraysTest.java
@@ -0,0 +1,27 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.StrictlyIdenticalArrays;
+
+/**
+ * @author Tony Liang
+ *
+ */
+public class StrictlyIdenticalArraysTest
+{
+	int[][] list1Lists = { { }, { 5, 2, 5, 6, 1, 6 }, { 5, 2, 5, 6, 6, 1 }, { 2 } };
+	int[][] list2Lists = { { }, { 5, 2, 5, 6, 1, 6 }, { 5, 2, 5, 6, 1, 6 }, { 1, 2 } };
+	boolean[] equalOrNot = { true, true, false, false };
+	
+	@Test
+	public void testEqual()
+	{
+		for (int list = 0; list < list1Lists.length; list++)
+		{
+			assertEquals(equalOrNot[list], StrictlyIdenticalArrays.equal(list1Lists[list], list2Lists[list]));
+		}
+	}
+}
